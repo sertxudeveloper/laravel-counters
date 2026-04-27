976e53946d0b5220239ca2ff100b93daf1fe1fdb
Sergio Peris <dev.sertxu@gmail.com>
2026-04-27 18:20:01 +0200

    wip

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,21 +1,17 @@
 <?php
 
-namespace Tests;
+declare(strict_types=1);
+
+namespace SertxuDeveloper\Counters\Tests;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Orchestra\Testbench\TestCase as Orchestra;
+use Orchestra\Testbench\TestCase as BaseTestCase;
 use SertxuDeveloper\Counters\CounterServiceProvider;
 
-class TestCase extends Orchestra
+abstract class TestCase extends BaseTestCase
 {
     use RefreshDatabase;
 
-    /**
-     * Get package providers.
-     *
-     * @param  \Illuminate\Foundation\Application  $app
-     * @return array<int, class-string>
-     */
     protected function getPackageProviders($app): array
     {
         return [
